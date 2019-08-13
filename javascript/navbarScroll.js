c6a2979174a8d70d5db08de79435bb32d73c967a
manas2297 <yadavmanas22@gmail.com>
2019-08-13 19:08:27 +0530

    card section

--- javascript/navbarScroll.js
+++ javascript/navbarScroll.js
@@ -4,5 +4,15 @@ $(window).scroll(()=>{
     }else{
         $('.nav').removeClass('navShadow');
     }
+
+    if($(window).scrollTop()>210){
+        $('#about').removeClass('active');
+        $('#contact').addClass('active');
+
+    }else if($(window).scrollTop()<210){
+        $('#about').addClass('active');
+        $('#contact').removeClass('active');
+    }
+    
     
 })
